7d6e53f85f676b3ff54f7bb8838a80d8b260847f
Olga <olga_bezrukova@bk.ru>
2024-01-25 16:41:07 +0300

    commit

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 const root = document.getElementById('root')
 
 const wrapper = document.createElement('div')
-const form = document.createElement('div')
-form.classList = 'form'
-root.append(form)
+const block = document.createElement('div')
+block.classList = 'block'
+root.append(block)
 
-const header = document.createElement('header')
-header.className = 'header'
-form.prepend(header)
+const form = document.createElement('form')
+form.className = 'form'
+block.prepend(form)
 
 const addTasks = document.createElement('button')
 addTasks.className = 'button'
 addTasks.type  = 'submit'
 addTasks.textContent = 'Add Task'
-header.prepend(addTasks)
+form.prepend(addTasks)
 
 const input = document.createElement('input')
 input.placeholder = 'Enter todo'
 input.type = 'text'
 input.classList.add('main-input')
-header.prepend(input)
+form.prepend(input)
 
 const deleteTasks = document.createElement('button')
 deleteTasks.className = 'button'
 deleteTasks.type  = 'reset'
 deleteTasks.textContent = 'Delete All'
-header.prepend(deleteTasks)
+form.prepend(deleteTasks)
 
 const createElement = (tagName, className) => {
     const element = document.createElement(tagName);
@@ -53,78 +53,84 @@ const createElementWithText = (tagName, className, textContent) => {
     return element;
 }
 
-const createSection = () => {
+const createSection = (text, date) => {
     const tasksSection = createElement('section', 'tasks-section');
+    tasksSection.id = self.crypto.randomUUID()
 
     const headerSection = createElement('div', 'header-section');
     tasksSection.append(headerSection)
 
     const closeTask = createElementButton('button', '✕', 'close-button');
+    createCloseButtonListener(closeTask);
     headerSection.append(closeTask);
 
     const centerSection = createElement('div', 'center-section');
     tasksSection.append(centerSection);
 
-    const addTask = createElementCheckbox('checkbox', false);
-    centerSection.append(addTask);
+    const acceptTask = createElementCheckbox('checkbox', false);
+    centerSection.append(acceptTask);
 
-    const toDoText = createElementWithText('div', 'toDoText', 'Learn JS');
-    toDoText.id = "id";
+    const toDoText = createElementWithText('div', 'toDoText', text);
     centerSection.append(toDoText);
 
-    const date = createElementWithText('span', 'dateTime', '2022.02.22 22:22:22');
-    tasksSection.append(date);
+    const dateTask = createElementWithText('span', 'dateTime', date);
+    tasksSection.append(dateTask);
 
     return tasksSection;
 }
 
-let date = new Date().toLocaleTimeString()
 
 
 
+let tasks = []
 
-
-
-
-
-
-
-
-// кнопка addTask
-
-addTasks.addEventListener('click', (event) => {
-    const addItem = createSection()
-    form.append(addItem)
-})
-
-// кнопка DeleteAll
+// кнопка deleteAll
 
 deleteTasks.addEventListener('click', (event) => {
-    confirm('Уверены,что хотите удалить все задания?')
-    for (step = 0;; step++) {
+    confirm('Are you sure you want to delete all the tasks?')
+    for (i = 0; i < tasks.length; i++) {
         document.querySelector('.tasks-section').remove()
     }
 })
 
+// добавление элементов
 
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const inputValue = event.currentTarget.elements[1].value;
+  if (inputValue === '') {
+    return;
+  }
+  let date = new Date().toLocaleTimeString()
 
+  const taskItem = createSection(inputValue, date)
+  tasks.push(taskItem)
+  block.append(taskItem)
 
-const tasks = []
-
-header.addEventListener('submit', (event) => {
-    event.preventDefault();
-    const inputValue = "learn JS";
-
-    const task = {
-    id:self.crypto.randomUUID(),
-    task: inputValue,
-    isCompleted: true,
-    date: new Date().toLocaleTimeString()
-}
-    tasks.push(task)
-    tasks.forEach(task => {
-    const taskItem = createSection()
-    form.append(taskItem)
+  event.target.reset();
 })
-event.target.reset();
-})                        //вообще не поняла, как это должно работать и работает ли на header
+
+// кнопка удалить конкретное задание
+
+function createCloseButtonListener(closeTask) {
+    closeTask.addEventListener('click', (event) => {
+    confirm('Do you want to delete this task?')
+    const identifier = event.currentTarget.closest('.tasks-section')
+    tasks = tasks.filter((taskSection) => taskSection.id !== identifier)
+    event.currentTarget.closest('.tasks-section').remove()
+    })
+}
+
+
+
+function createCheckboxListener(acceptTask) {
+    acceptTask.addEventListener('change', (event) => {
+        if (event.target.checked === true) {
+            document.querySelector('.toDoText').classList.add('del')
+        }
+    })
+}
+
+
+
+
